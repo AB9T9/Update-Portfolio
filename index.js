9397e9355e96302d9cab89749c2a787fba8454f9
AB SIDDIK <abft102ctg@gmai.com>
2026-08-03 09:53:32 +0600

    new features added

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ form.addEventListener("submit", (e) => {
 
   form.reset();
 });
+
+const html = document.documentElement;
+const toggle = document.getElementById("themeToggle");
+
+// Load saved theme
+const savedTheme = localStorage.getItem("theme") || "light";
+html.setAttribute("data-bs-theme", savedTheme);
+toggle.checked = savedTheme === "dark";
+
+// Toggle theme
+toggle.addEventListener("change", () => {
+  const theme = toggle.checked ? "dark" : "light";
+
+  html.setAttribute("data-bs-theme", theme);
+  localStorage.setItem("theme", theme);
+});
+
+const copyBtn = document.getElementById("copyBtn");
+const email = document.getElementById("email");
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(email.textContent);
+
+  copyBtn.textContent = "Copied!";
+
+  setTimeout(() => {
+    copyBtn.textContent = "Copy";
+  }, 2000);
+});
+document.getElementById("year").textContent = new Date().getFullYear();
